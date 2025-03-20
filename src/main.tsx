1e90cd680c18b00c7712a38be2571a026bb7a3c4
Vedant01 <vedant.agarwal312@gmail.com>
2025-03-20 14:16:16 +0530

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/project/">
         <App />
       </BrowserRouter>
     </Provider>
